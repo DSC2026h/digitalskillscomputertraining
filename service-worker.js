4509e7ef3c6b2d203a6d4c1b7fe4b79a2ce9ce13
DSC2026h <Digitalskillscomputertrainingh@gmail.com>
2026-08-29 07:05:37 +0100

    Update service worker to version 2 and improve caching

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,36 @@
-const CACHE_NAME = "digital-skills-v1";
+const CACHE_NAME = "digital-skills-v2";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
+  "./app.html",
   "./manifest.json",
   "./Logo.jpg"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
